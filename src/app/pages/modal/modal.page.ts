f262f742373237d07d69b1e8277d2ccbe85aaca5
Eloy Ortiz <ortizpulido.eloy@gmail.com>
2020-11-17 21:09:05 +0100

    Fin sección 6 android

--- src/app/pages/modal/modal.page.ts
+++ src/app/pages/modal/modal.page.ts
@@ -30,6 +30,7 @@ export class ModalPage implements OnInit {
     // const { data } = await modal.onDidDismiss();
     // console.log('onWillDismiss');
     console.log(data);
+    console.log('JSON.stringify(data) :>> ', JSON.stringify(data));
 
   }
 
